c06ed564d5f0340ca1d4444873cd04d30427ac4d
roman <romansheikh170@gmail.com>
2021-10-18 13:48:25 +0600

    add more components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Components/Pages/Home/Home/Home';
+import OurServices from './Components/Pages/OurServices/OurServices';
+import ServiceDetails from './Components/Pages/ServiceDetails/ServiceDetails';
+import Welldone from './Components/Pages/Welldone/Welldone';
 import Footer from './Components/Sheared/Footer/Footer';
 import NavBar from './Components/Sheared/NavBar/NavBar';
 
@@ -10,6 +14,19 @@ function App() {
         <NavBar></NavBar>
         <Switch>
           <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/services">
+            <OurServices></OurServices>
+          </Route>
+          <Route exact path="/ServiceDetails/:serviceID">
+            <ServiceDetails></ServiceDetails>
+          </Route>
+          <Route exact path="/welldone">
+            <Welldone></Welldone>
           </Route>
         </Switch>
         <Footer></Footer>
